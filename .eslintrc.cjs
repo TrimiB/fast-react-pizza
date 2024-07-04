c977274b75aa0bd65aae8f8e6c5df1ce365edb34
Kushtrim <bujupikushtrim@outlook.com>
2024-07-04 15:38:45 +0200

    added rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,8 +13,9 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'no-unused-vars': 'warn',
     'react/prop-types': 'off',
+    'react/no-unescaped-entities': 'warn',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
 };
