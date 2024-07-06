317c8fe7df8674f96f3ceb87b5e65fe869748554
Kushtrim <bujupikushtrim@outlook.com>
2024-07-06 21:51:38 +0200

    Created a header component.

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to='/'>Fast React Fizza Co.</Link>
+      <p>Kurt</p>
+    </header>
+  );
+}
+
+export default Header;
