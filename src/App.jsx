6b7629d5c24cb953a426b593bad55db215347156
Kushtrim <bujupikushtrim@outlook.com>
2024-07-06 21:52:17 +0200

    Nesting routes inside the AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,35 @@ import Menu from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
+import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/menu',
-    element: <Menu />,
-  },
-  {
-    path: '/cart',
-    element: <Cart />,
-  },
-  {
-    path: '/order/new',
-    element: <CreateOrder />,
-  },
-  {
-    path: '/order/:orderId',
-    element: <Order />,
+    element: <AppLayout />,
+    children: [
+      {
+        // it renders this element
+        path: '/',
+        // when the URL matches this segment
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+      },
+    ],
   },
 ]);
 
