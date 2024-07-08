98e88b85f7233b1c0c541279351e81f260847b48
Kushtrim <bujupikushtrim@outlook.com>
2024-07-08 22:48:25 +0200

    implementing errorElement and rendering Error component in case of an error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './ui/Home';
+import Error from './ui/Error';
 import Menu, { menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
@@ -9,6 +10,7 @@ import AppLayout from './ui/AppLayout';
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         // it renders this element
@@ -20,6 +22,7 @@ const router = createBrowserRouter([
         path: '/menu',
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: '/cart',
