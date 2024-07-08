0f4cb699caf643a735c11cc5ef890549db1626c8
Kushtrim <bujupikushtrim@outlook.com>
2024-07-08 21:33:30 +0200

    using useNavigation to use the navigation state in order to display loading indicator when data is fetched

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
 import Header from './Header';
+import Loading from './Loading';
 import CartOverview from '../features/cart/CartOverview';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isloading = navigation.state === 'loading';
+
   return (
-    <div>
+    <div className='layout'>
+      {isloading && <Loading />}
+
       <Header />
 
       <main>
