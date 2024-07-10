72a46e79d2a2558f36d689ce9fc1bdf0c2e20830
Kushtrim <bujupikushtrim@outlook.com>
2024-07-10 22:40:08 +0200

    using the action: createOrderAction

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './ui/Home';
 import Error from './ui/Error';
 import Menu, { menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, { createOrderAction } from './features/order/CreateOrder';
 import Order, { orderLoader } from './features/order/Order';
 import AppLayout from './ui/AppLayout';
 
@@ -31,6 +31,8 @@ const router = createBrowserRouter([
       {
         path: '/order/new',
         element: <CreateOrder />,
+        action: createOrderAction,
+        errorElement: <Error />,
       },
       {
         path: '/order/:orderId',
