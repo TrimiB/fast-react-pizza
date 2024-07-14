609ecb19f8d0338b4f651ca086360b986827915b
Kushtrim <bujupikushtrim@outlook.com>
2024-07-14 22:12:08 +0200

    refactor to named export

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+
+const config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+export default config;
