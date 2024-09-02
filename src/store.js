fb7ea99356b601fbd6043f2368b70ef5a1dd23f2
Kushtrim <bujupikushtrim@outlook.com>
2024-09-02 22:08:32 +0200

    [added] Comments

--- src/store.js
+++ src/store.js
@@ -3,6 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/user/userSlice';
 import cartReducer from './features/cart/cartSlice';
 
+/**
+ * Configures and exports the Redux store for the application.
+ * The store is configured with two reducers:
+ * - `userReducer` from the `userSlice` module
+ * - `cartReducer` from the `cartSlice` module
+ *
+ * This store can be imported and used throughout the application to access and update the global state.
+ */
 const store = configureStore({
   reducer: {
     user: userReducer,
