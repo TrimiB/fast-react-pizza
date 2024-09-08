e0d8c6a742bf937c1b7ce9732b51a9d062ed3079
Kushtrim <bujupikushtrim@outlook.com>
2024-09-08 22:55:22 +0200

    [using] useFetcher to fetch/load data from an diferent route

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,17 +1,27 @@
 // Test ID's: IIDSAT / CQE92U
 
 import OrderItem from './OrderItem';
-import { useLoaderData } from 'react-router-dom';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from '../../utilities/helpers';
+import { useEffect } from 'react';
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna exclude names or address, these are only for the restaurant staff
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+    },
+    [fetcher],
+  );
 
   const {
     id,
@@ -55,7 +65,15 @@ function Order() {
 
       <ul className='divide-y divide-stone-200 border-b border-t border-stone-200'>
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher?.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((element) => element.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
