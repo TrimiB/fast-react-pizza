b7451454b1d193abad817ae77b5de57c7dd730cd
Kushtrim <bujupikushtrim@outlook.com>
2024-09-08 13:29:17 +0200

    [add] apostroph

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -34,7 +34,9 @@ function CreateOrder() {
 
   return (
     <div className='px-4 py-6'>
-      <h2 className='mb-8 text-xl font-semibold'>Ready to order? Let's go!</h2>
+      <h2 className='mb-8 text-xl font-semibold'>
+        Ready to order? Let&apos;s go!
+      </h2>
 
       <button onClick={() => dispatch(fetchAddress())}>get my location</button>
 
